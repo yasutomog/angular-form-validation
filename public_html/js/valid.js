0bc32dc95e08c73c4184e417620fb1b4c50baf4d
YasutomoGoto <goto@anti.com>
2014-09-01 11:38:56 +0900

    不要なコード削除

--- public_html/js/valid.js
+++ public_html/js/valid.js
@@ -1,12 +1,6 @@
 var ValidApp = angular.module('ValidApp', []);
 ValidApp.controller('ValidController', ['$scope', function($scope) {
 
-	$scope.user = {
-		name: '',
-		username: '',
-		email: ''
-	};
-
     $scope.submitForm = function(isValid) {
         if (isValid) {
             //Persist data
@@ -14,7 +8,6 @@ ValidApp.controller('ValidController', ['$scope', function($scope) {
     };
 
     $scope.send = function() {
-        debugger;
         alert($scope.user.name + '::' + $scope.user.username + '::' + $scope.user.email);
     };
 }]);
