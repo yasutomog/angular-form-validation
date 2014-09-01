fd9d3f7e002043b937ef0dea737245dc5cdccb86
YasutomoGoto <goto@anti.com>
2014-09-01 17:26:34 +0900

    リセットボタンの処理実装

--- public_html/js/valid.js
+++ public_html/js/valid.js
@@ -1,13 +1,31 @@
 var ValidApp = angular.module('ValidApp', []);
 ValidApp.controller('ValidController', ['$scope', function($scope) {
 
+	$scope.defUser = {
+		name: 'koichi sato',
+		username: '',
+		email: ''
+	};
+
     $scope.submitForm = function(isValid) {
         if (isValid) {
-            //Persist data
+            //　Persist data
         }
     };
 
-    $scope.send = function() {
-        alert($scope.user.name + '::' + $scope.user.username + '::' + $scope.user.email);
+    $scope.inUnChanged = function(user) {
+    	return angular.equals(user, $scope.defUser);
     };
+
+    $scope.reset = function() {
+    	$scope.user = angular.copy($scope.defUser);
+    };
+
+    $scope.send = function(user) {
+        alert($scope.user.name + '::' + $scope.user.username + '::' + $scope.user.email);
+        $scope.defUser = angular.copy(user);
+    };
+
+    $scope.reset();
+
 }]);
